4c08960a28dac1cbd58f949d68a84e501d6fe7f8
NuvBlackone <esequielalbornoz7@gmail.com>
2018-10-05 23:46:41 -0300

    pong v1

--- pong/pong.cpp
+++ pong/pong.cpp
@@ -18,9 +18,157 @@ void gotoxy(int x, int y){
   
 }
 
+class Player{
+	
+	private:
+		
+		int posX;
+		int posY;
+		int size;
+		int score;
+		
+	public:
+		
+		Player(int dx, int dy, int size);
+		void draw(int move);
+		int get_pos();
+		void set_score(int sc);
+		int get_score();
+		
+};
+
+Player::Player(int dx, int dy, int size){
+	
+	this->posX = dx;
+	this->posY = dy;
+	this->size = size;
+	this->score = 0;
+	
+}
+
+int Player::get_pos(){
+	
+	return this->posY;
+}
+
+void Player::set_score(int sc){
+	
+	this->score += sc;
+}
+
+int Player::get_score(){
+	
+	return this->score;
+}
+
+void Player::draw(int move){
+	
+	
+	if (move == 1){
+		
+		if (this->posY + this->size != 21){
+			
+			int dy = this->posY;
+			this->posY += 1;
+			
+			for (int i = this->posY; i < this->posY + this->size - 1; i++){
+				
+				gotoxy(this->posX, i); printf("%c", 219);
+			}
+			
+			gotoxy(this->posX, this->posY - this->size + 2); printf(" ");
+			
+		}
+		
+	}else if (move == 0){
+		
+		if (this->posY - 1 != 3){
+			
+			this->posY -= 1;
+			
+			for (int i = this->posY; i < this->posY + this->size - 1; i++){
+				
+				gotoxy(this->posX, i); printf("%c", 219);
+			}
+			
+			gotoxy(this->posX, this->posY + this->size - 1); printf(" ");
+		}
+		
+		
+	}else{
+		
+		for (int i = this->posY; i < this->posY + this->size - 1; i++){
+			
+			gotoxy(this->posX, i); printf("%c", 219);
+		}
+	}
+}
+
+class Ball{
+	
+	private:
+		
+		float posX;
+		float posY;
+		float dx;
+		float dy;
+		
+	public:
+		
+		Ball(int _dx, int _dy);
+		int draw(Player p1, Player p2);
+};
+
+
+Ball::Ball(int _dx, int _dy){
+	
+	this->posX = 34;
+	this->posY = 15;
+	this->dx = _dx;
+	this->dy = _dy;
+}
+
+int Ball::draw(Player p1, Player p2){
+	
+	int posP1 = p1.get_pos();
+	int posP2 = p2.get_pos();
+	
+	gotoxy(this->posX, this->posY); printf("%c", 32);
+	
+	if (this->posX >= 66){
+		
+		return 1;
+		
+	}else if (this->posX <= 5){
+	
+		return 2;
+	}
+	
+	if (this->posY >= 19 || this->posY <= 4){
+		this->dy *= -1;
+	}
+	
+	
+	if ( this->posX  == 6 && (this->posY >= posP1 && this->posY <= posP1 + 2)){
+		this->dx *= -1;		
+		
+	}else if ( this->posX  == 64 && (this->posY >= posP2 && this->posY <= posP2 + 2)){
+		
+		this->dx *= -1;		
+	}
+	
+	this->posX += dx;
+	this->posY += dy;
+	
+	gotoxy(this->posX, this->posY); printf("%c", 248);
+	
+	return 0;
+
+}
+
 void console_config(){
 	
-	//Resize console
+	//RESIZE CONSOLE
 	
 	HWND console = GetConsoleWindow();
 	RECT r;
@@ -28,7 +176,7 @@ void console_config(){
 	
 	MoveWindow(console, r.left + 350, r.top + 50, WIDTH, HIGH, TRUE);
 	
-	//Hide cursor
+	//HIDE CURSOR
 	
 	HANDLE hOut;
 	CONSOLE_CURSOR_INFO ConCurInf;
@@ -43,7 +191,7 @@ void console_config(){
 
 void draw_map(){
 	
-	//draw vertical and horizontal walls
+	//DRAW VERTICAL AND HORIZONTAL WALLS
 	
 	for (int i = 4; i < 67; i++){
 		
@@ -66,7 +214,7 @@ void draw_map(){
 		gotoxy(i, 20); printf("%c", 205);
 	}
 	
-	//draw corners
+	//DRAW CORNERS
 	
 	gotoxy(3, 3); printf("%c", 201);
 	gotoxy(67, 3); printf("%c", 187);
@@ -75,25 +223,67 @@ void draw_map(){
 	
 }
 
+void draw_score(Player p1, Player p2){
+	
+	int score1 = p1.get_score();
+	int score2 = p2.get_score();
+	
+	gotoxy(4, 2); printf("Score: %i", score1);
+	gotoxy(58, 2); printf("Score: %i", score2);
+}
+
 int main(){
 	
+	//CONFIGURE CONSOLE
 	console_config();
 	
+	//DRAW MAP
 	draw_map();
-	Ball ball = Ball(35, 13);
-	Player player1 = Player(3);
+	
+	//CREATE BALL
+	Ball ball = Ball(2, 1);
+	
+	//CREATE PLAYERS
+	
+	Player p1 = Player(5, 12, 3);
+	Player p2 = Player(65, 12, 3);
+	
+	//PRE-LOAD PLAYERS
+	p1.draw(-1);
+	p2.draw(-1);
+	
+	int loop = 0;
 	
 	while(1){
 		
-		ball.draw();
+		//DRAW BALL	
 		
-		if (GetAsyncKeyState(VK_UP)){
-						
-		}else if (GetAsyncKeyState(VK_DOWN)){
+		if (loop >= 1){
+			int point = ball.draw(p1, p2);
+			loop = 0;
 			
-					
+			if (point == 1){
+				
+				p1.set_score(1);
+				ball = Ball(2, -1);
+				
+			}else if (point == 2){
+				
+				p2.set_score(1);
+				ball = Ball(-2, -1);
+				
+			}
 		}
 		
+		draw_score(p1, p2);
+		
+		//PLAYER 1 MOVE
+		if (GetAsyncKeyState(0x57)) p1.draw(0); else if (GetAsyncKeyState(0x53)) p1.draw(1);
+		
+		//PLAYER 2 MOVE
+		if (GetAsyncKeyState(VK_UP)) p2.draw(0); else if (GetAsyncKeyState(VK_DOWN)) p2.draw(1);
+		
+		loop++;		
 		Sleep(50);
 	}
 	
